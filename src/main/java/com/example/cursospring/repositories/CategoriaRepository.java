2bc735d902aea7c7b14ed9094eff9741c6314bb7
Ricardo Murca <richard_sm1@outlook.com>
2021-07-27 17:37:39 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/example/cursospring/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.example.cursospring.repositories;
+
+import com.example.cursospring.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
