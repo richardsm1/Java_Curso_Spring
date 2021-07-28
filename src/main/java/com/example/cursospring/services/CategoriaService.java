7eadba25624f48bb80c5abf0571da253b68b9bf7
Ricardo Murca <richard_sm1@outlook.com>
2021-07-28 15:55:25 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/example/cursospring/services/CategoriaService.java
+++ src/main/java/com/example/cursospring/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.example.cursospring.services;
 
 import com.example.cursospring.domain.Categoria;
 import com.example.cursospring.repositories.CategoriaRepository;
+import com.example.cursospring.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class CategoriaService {
 
     public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() ->
+                new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
